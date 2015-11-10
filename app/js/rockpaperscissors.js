a597a5d3786162a11c9eedfde4751cb41b1a5ab7
monq <forgetmenaught@gmail.com>
2015-11-09 23:21:48 -0800

    fill-in getWinner() plus helper enumerator() functions.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -31,7 +31,7 @@ function getPlayerMove(move) {
 	return move = move || getInput();
 }
 
-// 
+// String -> String
 function getComputerMove(move) {
 	// Write an expression that operates on a variable called `move`
 	// If a `move` has a value, your expression should evaluate to that value.
@@ -39,12 +39,30 @@ function getComputerMove(move) {
 	return move = move || randomPlay();
 }
 
+// String String -> String
 function getWinner(playerMove,computerMove) {
 	var winner;
 	// Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
 	// Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
 	// The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
 	/* YOUR CODE HERE */
+	
+	// enumerate the moves. <see Helper Functions below>
+	var playerResult = enumerator(playerMove);
+	var compResult = enumerator(computerMove);
+	
+	if (playerResult === compResult) {
+		winner = "tie";
+	} else if (playerMove === 2 && compResult === 0) {
+		winner = "player";
+	} else if (compResult === 2 && playerResult === 0) {
+		winner = "computer";
+	} else if (playerMove < computerMove) {
+		winner = "player"
+	} else {
+		winner = "computer"
+	}
+
 	return winner;
 }
 
@@ -57,3 +75,17 @@ function playToFive() {
 	return [playerWins, computerWins];
 }
 
+/* Helper Functions */
+
+// String -> Number[0, 2]
+// enumerator returns 0 for rock, 1 for paper, 2 for scissors.
+function enumerator (move) {
+	switch (move) {
+	case 'rock':
+		 return 0;
+	case 'paper':
+		return 1;
+	case 'scissors':
+		return 2;
+	}
+}
